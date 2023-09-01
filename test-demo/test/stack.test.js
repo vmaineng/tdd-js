630c015932232a34f617772cb49f00af3c0e3342
Mai Vang <vmaineng@gmail.com>
2023-09-01 13:25:39 -0700

    added in test statements

---
+++ test-demo/test/stack.test.js
@@ -0,0 +1,14 @@
+//describe - define a test suite; describe what it is you're testing,
+//takes a CB as a second argument, which we can define multiple individual tests
+
+//test/it = individual tests
+
+describe('My Stack', () => {
+
+    it('is created empty');
+
+    it('can push to the top');
+
+    it('can pop off');
+
+})
