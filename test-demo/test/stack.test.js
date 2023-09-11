086cb6fdf65c44b79066c6705fbc9ef64734fbe7
Mai Vang <vmaineng@gmail.com>
2023-09-11 16:14:47 -0700

    added in stack constructor

--- test-demo/test/stack.test.js
+++ test-demo/test/stack.test.js
@@ -1,7 +1,14 @@
 //describe - define a test suite; describe what it is you're testing,
 //takes a CB as a second argument, which we can define multiple individual tests
 
-//test/it = individual tests
+//test - it = individual tests
+
+class Stack {
+    constructor() {
+        this.top = -1;
+        this.items = {};
+    }
+}
 
 describe('My Stack', () => {
 
